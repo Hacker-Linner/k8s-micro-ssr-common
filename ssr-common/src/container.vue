5796984ce54592fe12b6ed820570726eec401812
Kirk-Wang <kirk.w.wang@gmail.com>
2020-12-06 23:09:50 +0800

    update

--- ssr-common/src/container.vue
+++ ssr-common/src/container.vue
@@ -76,7 +76,7 @@ export default Vue.extend<Data, Methods, Computed>({
         async serverFetch(ssrname: string) {
             const renderUrl = encodeURIComponent(this.$route.fullPath);
             const res = await axios.get(
-                `http://localhost:3000/api/${ssrname}/render`,
+                `${process.env.SSR_DOMAIN || 'http://localhost:3000'}/api/${ssrname}/render`,
                 {
                     params: {
                         routerMode: 'history',
